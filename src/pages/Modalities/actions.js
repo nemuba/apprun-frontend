178b77b59db43acd71d346e272a51a765e9fd3e5
Alef Ojeda de Oliveira <nemubatubag@gmail.com>
2020-05-10 19:54:55 -0300

    Add validations in modalities

--- src/pages/Modalities/actions.js
+++ src/pages/Modalities/actions.js
@@ -9,7 +9,6 @@ export const fetchModalitiesAsync = (id = 0 ) =>{
       .get(url)
       .then((resp) => dispatch(fetch_modalities(id !== 0 ? [resp.data] : resp.data)))
       .catch((erro) =>{
-        console.log(erro);
         toast.warn(
           "Não foi possivel buscar as modalidades contate um administrador"
         );
@@ -22,7 +21,13 @@ export const addModalityAsync = (form) =>{
   return (dispatch) => {
     api
     .post("/modalities", {modality: form})
-    .then(()=> toast.success("Cadastrado com sucesso!"))
+    .then((resp)=> {
+      if(resp.status === 201){
+        toast.success("Cadastrado com sucesso!");
+      }else if(resp.status === 203){
+        toast.warn(resp.data[0]);
+      }
+    })
     .catch(()=> toast.warn("Não foi possivel cadastrar a modalidade contate o administrador do sistema!"))
   }
 }
@@ -31,7 +36,13 @@ export const updateModalityAsync = (id, form) => {
   return (dispatch) => {
     api
       .patch(`/modalities/${id}`, { modality: form })
-      .then(() => toast.success("Atualizado com sucesso!"))
+      .then((resp) => {
+        if(resp.status === 200){
+          toast.success("Atualizado com sucesso!");
+        }else if(resp.status === 203){
+          toast.warn(resp.data[0]);
+        }
+      })
       .catch(() =>
         toast.warn(
           "Não foi possivel atualizar a modalidade contate o administrador do sistema!"
