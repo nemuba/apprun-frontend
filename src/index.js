678379ae227fda244775d1694b09f505e52ca9d9
Alef Ojeda de Oliveira <nemubatubag@gmail.com>
2020-07-26 12:53:37 -0300

    remove persit store

--- src/index.js
+++ src/index.js
@@ -4,19 +4,16 @@ import { Provider } from 'react-redux';
 import {ConnectedRouter} from 'connected-react-router';
 import {history} from './store/index';
 import App from './App';
-import { store, persistor } from './store';
-import {PersistGate} from 'redux-persist/lib/integration/react';
+import { store } from './store';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "react-toastify/dist/ReactToastify.min.css";
 import './index.css';
 
 ReactDOM.render(
   <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
       <ConnectedRouter history={history}>
         <App />
       </ConnectedRouter>
-    </PersistGate>
   </Provider>,
   document.getElementById("root")
 );
--- src/store/index.js
+++ src/store/index.js
@@ -2,29 +2,16 @@ import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import {createBrowserHistory} from 'history';
 import {routerMiddleware} from 'connected-react-router';
 
-import { persistReducer, persistStore } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-
 
 import reducers from './rootReducer';
 
 export const history = createBrowserHistory();
 
-const persistConfig = {
-  key: 'auth',
-  storage,
-  whitelist: ['auth','filter_modality']
-}
-
-const persistedReducer = persistReducer( persistConfig,reducers(history));
 
 // export store with reducers
-const store =  configureStore({
-  reducer: persistedReducer,
+export const store =  configureStore({
+  reducer: reducers(history),
   middleware: [...getDefaultMiddleware(), routerMiddleware(history)],
   devTools: process.env.NODE_ENV !== 'production'
 });
 
-const persistor = persistStore(store);
-
-export { store, persistor };
